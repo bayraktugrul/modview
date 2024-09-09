64fea032fcfce27f77baee558659e73e1f84f983
bayraktugrul <tugrulbyrk1@gmail.com>
2024-09-10 02:01:24 +0300

    Add running info

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/bayraktugrul/modview
 
 go 1.22.0
 
-require golang.org/x/mod v0.21.0
+require (
+	github.com/fatih/color v1.17.0
+	golang.org/x/mod v0.21.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,35 +2,55 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 
 	"github.com/bayraktugrul/modview/internal"
+
+	"github.com/fatih/color"
 )
 
 func main() {
+	color.Cyan("🚀 Starting modview...")
+	logo := `
+   __  ___        __     _           
+  /  |/  /__  ___/ /  __(_)__ _    __
+ / /|_/ / _ \/ _  / |/ / / -_) |/|/ /
+/_/  /_/\___/\_,_/|___/_/\__/|__,__/`
+	color.Cyan(logo)
+
 	cmd := exec.Command("go", "mod", "graph")
 	var out bytes.Buffer
 	cmd.Stdout = &out
 
 	if err := cmd.Run(); err != nil {
+		color.Red("❌ Error running 'go mod graph': %v", err)
 		panic(err)
 	}
 
+	color.Green("✅ 'go mod graph' command executed successfully.")
 	result, err := internal.Convert(strings.NewReader(out.String()))
 	if err != nil {
+		color.Red("❌ Error converting graph data: %v", err)
 		panic(err)
 	}
+
+	color.Green("✅ Graph data converted successfully.")
 
 	htmlContent := internal.GenerateHTML(result)
 	if err != nil {
-		fmt.Println("Error generating HTML:", err)
+		color.Red("❌ Error generating HTML: %v", err)
 		return
 	}
 
+	color.Green("✅ HTML content generated successfully.")
+
 	if err := os.WriteFile("dependency_tree.html", []byte(htmlContent), 0644); err != nil {
-		fmt.Println("Error writing HTML file:", err)
+		color.Red("❌ Error writing HTML file: %v", err)
+		return
 	}
+
+	color.Green("✅ HTML file 'dependency_tree.html' written successfully.")
+	color.Cyan("🎉 modview completed successfully.")
 }
